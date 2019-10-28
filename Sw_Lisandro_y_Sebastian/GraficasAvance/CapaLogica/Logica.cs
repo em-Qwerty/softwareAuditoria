706b5f2ff5c6bb99a50b6df94f144ff6ee27969d
Victor Fernández <victorlisandro0@gmail.com>
2019-10-28 03:50:10 -0600

    Funcion para contar subobjetivos fue agregada
    trri

--- Sw_Lisandro_y_Sebastian/GraficasAvance/CapaLogica/Logica.cs
+++ Sw_Lisandro_y_Sebastian/GraficasAvance/CapaLogica/Logica.cs
@@ -23,7 +23,7 @@ namespace CapaLogica
         public void cargarDominios(object sender, EventArgs e, System.Windows.Forms.ComboBox cmbx, string consulta)
          {
             // 27/10/2019 Autor: Victor Fernandez
-            // Esta funcion carga los resultados de una columna (de uan consulta) al
+            // Este metodo carga los resultados de una columna (de uan consulta) al
             // combobox
 
             try
@@ -47,5 +47,44 @@ namespace CapaLogica
                 MessageBox.Show(ex.Message);
             }
         }
+
+
+
+        public int contarSubobjetivos(int idObjetivo)
+        {
+            // 27/10/2019 Autor: Victor Fernandez
+            // Esta funcion devuelve el numero de subobjetivos dependiendo el objetivo 
+
+            //SELECT COUNT(Nombre) AS NumeroSubobjetivos
+            //FROM tbl_subobjetivo
+            //WHERE tbl_objetivo_PK_Id_objetivo = 1;
+
+            int totalSubobjetivos = 0;
+
+            try
+            {
+                ConectarServidor cn = new ConectarServidor();
+                string comando = "SELECT COUNT(Nombre) AS NumeroSubobjetivos FROM tbl_subobjetivo WHERE tbl_objetivo_PK_Id_objetivo =" + idObjetivo;
+
+                OdbcCommand command = new OdbcCommand(comando, cn.Conexion());
+                OdbcDataReader queryResultsReader = command.ExecuteReader();
+
+                while (queryResultsReader.Read())
+                {
+                   // Conviertiendo el resultado a entero para poder enviarlo en el return 
+                   totalSubobjetivos = Convert.ToInt32(queryResultsReader.GetString(0));
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                // Mostrando mensaje que indica el error 
+                MessageBox.Show(ex.Message);
+            }
+
+            return totalSubobjetivos;
+        }
+
     }
 }
