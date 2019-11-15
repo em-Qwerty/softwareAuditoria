aef77ba44472239059dfcbbfcd38075705e170ab
victorlisandro0 <victorlisandro0@gmail.com>
2019-11-14 23:48:44 -0600

    Graficas-avance

---
+++ Sw_Lisandro_y_Sebastian/GraficasAvance/CapaDatos/Conexion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.Odbc;
+
+
+
+namespace CapaDatos
+{
+    public class ConectarServidor
+    {
+        public OdbcConnection Conexion()
+        {
+            OdbcConnection conectar = new OdbcConnection("Dsn=dsnauditoria;");
+            try
+            {
+                conectar.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return conectar;
+        }
+        public OdbcConnection CerrarConexion()
+        {
+            OdbcConnection conectar = new OdbcConnection("Dsn=dsnauditoria;");
+            try
+            {
+                conectar.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return conectar;
+        }
+    }
+}
